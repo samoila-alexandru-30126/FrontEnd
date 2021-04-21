732c6308610fa116eb5cd7c67d34448d2e465589
Adi Samoila <alexsamoila1998@gmail.com>
2021-04-21 13:05:36 +0300

    First commit

---
+++ frontend/js/app.js
@@ -0,0 +1,72 @@
+
+const  axios  = require('axios').default;
+
+var Admin = [
+{
+  username:"admin",
+  password:"admin"
+},
+{
+  username:"Alex",
+  password:"12345"
+}
+];
+function deletebyId()
+{
+  var Id1=document.getElementById("Id1").value;
+  
+}
+function ShowCategories()
+{
+
+  axios.get("http://localhost:8080/categories/2").then((response) => {console.log(response.data)
+})
+.catch(err =>{
+  console.error(err);
+}
+  
+  )
+}
+function openPage(pageName, elmnt, color) {
+  // Hide all elements with class="tabcontent" by default */
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent1");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the background color of all tablinks/buttons
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+  }
+
+  // Show the specific tab content
+  document.getElementById(pageName).style.display = "block";
+
+  // Add the specific color to the button used to open the tab content
+  elmnt.style.backgroundColor = color;
+}
+
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+function openCity(evt, cityName) {
+  // Declare all variables
+  var i, tabcontent, tablinks;
+
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  // Show the current tab, and add an "active" class to the link that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
